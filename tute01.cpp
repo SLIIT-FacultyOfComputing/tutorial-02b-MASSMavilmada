ec047d1dc675cea41e3dc9b2ef8e790b7ff3eaa6
Sanuth <>
2022-02-17 14:42:42 +0000

    -

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,33 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
-int main() 
+// #include <stdio.h>
+// int main() 
+// {
+//     float cm, inches;
+//     printf("Enter a length in cm : ");
+//     scanf("%f",&cm);
+//     inches = cm / 2.54;
+//     printf("Length in inches is %f \n", inches);
+//     return 0;
+// }  
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+
+    cout << "Enter a length in cm  : ";
+    cin >> cm;
+
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+
+    cout << "\nLength in inches is " << inches << endl;
+
     return 0;
-}  
+
+}
--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,29 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+
+long nCr(int n, int r)
+{
+    int facn = 1;
+    for(int x=n; x>=1; x--)
+    {
+        facn = facn * x;
+    }
+
+    int facr = 1;
+    for(int y=r; y>=1; y--)
+    {
+        facr = facr * y;
+    }
+
+    int NR = (n-r);
+    int facnr = 1;
+    for(int z=NR; z>=1; z--)
+    {
+        facnr = facnr * z;
+    }
+
+  return  ((facn/(facr * facnr)));
+
+}
